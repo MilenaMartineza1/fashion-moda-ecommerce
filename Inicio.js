7b48929c22bc374ace1ae38f3e4dd2ed0f7ed2f2
Milena MA <milenamartineza12@gmail.com>
2024-09-02 08:26:39 -0500

    actualizacion inicio

--- Inicio.js
+++
@@ -1,8 +0,0 @@
-let sliderInner = document.querySelector(".slider--Inner");
-
-let index = 1;
-
-setInterval(function(){
-    let percentage = index * -100;
-    sliderInner.style.transform =  "translateX("+ poncentage +"%)" index++;
-},1000)
