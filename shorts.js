6df00a86687431b85a204b06c5e9223fe3adda19
Milena MA <milenamartineza12@gmail.com>
2024-09-25 12:11:40 -0500

    Actualización productos

---
+++ shorts.js
@@ -0,0 +1,11 @@
+function currentSlide(element) {
+    if (element.classList.contains('active')) {
+        element.classList.remove('active');
+    } else {
+        var colors = document.querySelectorAll('.colors span');
+        colors.forEach(function(color) {
+            color.classList.remove('active');
+        });
+        element.classList.add('active');
+    }
+}
